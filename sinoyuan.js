80f83377bc1b277c5c19cab284fdaa337b3cf480
zsj2145676 <zsj.jacob@gmail.com>
2013-08-20 22:37:35 +0800

    update

--- sinoyuan.js
+++ sinoyuan.js
@@ -1,1 +1,17 @@
-alert("this is my first bookmarklets，and now let's start a new app");
+(function() {
+    var u = "http://shujian.me/",
+        s = u + "sinoyuanscript/", //http://vfancn.blob.core.chinacloudapi.cn/scripts/
+        q = u + "sinoyuancss/"; //http://vfancn.blob.core.chinacloudapi.cn/cfcss/
+    /* define some global variable */
+    var i = {
+        dir: u,
+        js_dir: s,
+        css_dir: q,
+        init: function() {
+            console.log(u);
+            console.log(s);
+            console.log(q);
+        }
+    };
+    i.init();
+})();
